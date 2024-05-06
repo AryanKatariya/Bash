3fee21453b7dada092d312d81e6650e3aab60a5f
AryanKatariya <aryankatariya21@gmail.com>
2024-05-06 23:23:17 +0530

    6/5/24

--- array/first.sh
+++ array/first.sh
@@ -7,6 +7,7 @@ courses[10]=DataScience
 courses[20]=Devops
 echo "First Element : ${courses[0]}"
 echo "Second Element : ${courses[1]}"
+echo "Fifth Element : ${courses[5]}"
 echo "All Elements with @ : ${courses[@]}"
 echo "All elements with * : ${courses[*]}"
 echo "The indices where elements are available : ${!courses[@]}"
