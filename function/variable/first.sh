88531128db7891902d78d7c4419d435c9bd0c244
AryanKatariya <aryankatariya21@gmail.com>
2024-05-13 23:44:34 +0530

    13/05/24

---
+++ function/variable/first.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#f1(){
+#	echo "x value:$x"
+#}
+#f1
+#x="aryan"
+#f1
+
+#f1(){
+#	x=10
+#}
+#echo "x value:$x"
+#f1
+#echo "x value:$x"
+
+
+#! /bin/bash
+f1()
+{
+ x=20
+ echo "x value : $x"
+}
+x=10
+echo "After f1 execution x value: $x"
+f1
+echo "After f1 execution x value: $x"
+
