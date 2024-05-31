fd5829f661fbad9585f862dcb9e359b5cecef5fc
AryanKatariya <aryankatariya21@gmail.com>
2024-05-31 23:44:32 +0530

    31/05/24

--- awk/script.sh
+++ awk/script.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+awk 'BEGIN{print "Beginning of the File";print"------------------";c=0;}{print 
+$0;c=c+1;}END{print ".........................";print "The Number Of Lines:" c}' input.txt
+
+awk 'BEGIN{print "Beginning of the File";print" ------------------ ";c=0;}{c=c+1;print c": "$0;}END{print ".........................";print "The Number Of Lines:"c}' input.txt
